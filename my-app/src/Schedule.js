6d78c29f58f6b2be29854934512083ac2a287da3
Owen Zhang <owenzhang2010@berkeley.edu>
2021-11-14 17:13:23 -0500

    full schedules for 4 default fighters

--- my-app/src/Schedule.js
+++ my-app/src/Schedule.js
@@ -1,8 +1,13 @@
 import './Schedule.css';
-import React, { Component } from 'react';
+import React, { Component, useDebugValue } from 'react';
 
-function isBlocked(schedId, blockedTimes) {
-    return blockedTimes.includes(schedId) ? 'blocked' : 'unblocked'
+function activityType(schedId, blockedTimes) {
+    for (const [activity, times] of Object.entries(blockedTimes)) {
+        if (times.includes(schedId)) {
+            return activity;
+        }
+    }
+    return 'unblocked';
 }
 
 class Schedule extends React.Component {
@@ -19,72 +24,72 @@ class Schedule extends React.Component {
                   Schedule
                   </th>
                 </tr>
-                <tr id="7" className={isBlocked("7", this.props.blockedTimes)}>
+                <tr id="7" className={activityType("7", this.props.blockedTimes)}>
                   <th>
                   7:00 AM
                   </th>
                 </tr>
-                <tr id="8" className={isBlocked("8", this.props.blockedTimes)}>
+                <tr id="8" className={activityType("8", this.props.blockedTimes)}>
                   <th>
                   8:00 AM
                   </th>
                 </tr>
-                <tr id="9" className={isBlocked("9", this.props.blockedTimes)}>
+                <tr id="9" className={activityType("9", this.props.blockedTimes)}>
                   <th>
                   9:00 AM
                   </th>
                 </tr>
-                <tr id="10" className={isBlocked("10", this.props.blockedTimes)}>
+                <tr id="10" className={activityType("10", this.props.blockedTimes)}>
                   <th>
                    10:00 AM
                   </th>
                 </tr>
-                <tr id="11" className={isBlocked("11", this.props.blockedTimes)}>
+                <tr id="11" className={activityType("11", this.props.blockedTimes)}>
                   <th>
                    11:00 AM
                   </th>
                 </tr>
-                <tr id="12" className={isBlocked("12", this.props.blockedTimes)}>
+                <tr id="12" className={activityType("12", this.props.blockedTimes)}>
                   <th>
                    12:00 PM
                   </th>
                 </tr>
-                <tr id="13" className={isBlocked("13", this.props.blockedTimes)}>
+                <tr id="13" className={activityType("13", this.props.blockedTimes)}>
                   <th>
                   1:00 PM
                   </th>
                 </tr>
-                <tr id="14" className={isBlocked("14", this.props.blockedTimes)}>
+                <tr id="14" className={activityType("14", this.props.blockedTimes)}>
                   <th>
                   2:00 PM
                   </th>
                 </tr>
-                <tr id="15" className={isBlocked("15", this.props.blockedTimes)}>
+                <tr id="15" className={activityType("15", this.props.blockedTimes)}>
                   <th>
                   3:00 PM
                   </th>
                 </tr>
-                <tr id="16" className={isBlocked("16", this.props.blockedTimes)}>
+                <tr id="16" className={activityType("16", this.props.blockedTimes)}>
                   <th>
                   4:00 PM
                   </th>
                 </tr>
-                <tr id="17" className={isBlocked("17", this.props.blockedTimes)}>
+                <tr id="17" className={activityType("17", this.props.blockedTimes)}>
                   <th>
                   5:00 PM
                   </th>
                 </tr>
-                <tr id="18" className={isBlocked("18", this.props.blockedTimes)}>
+                <tr id="18" className={activityType("18", this.props.blockedTimes)}>
                   <th>
                   6:00 PM
                   </th>
                 </tr>
-                <tr id="19" className={isBlocked("19", this.props.blockedTimes)}>
+                <tr id="19" className={activityType("19", this.props.blockedTimes)}>
                   <th>
                   7:00 PM
                   </th>
                 </tr>
-                <tr id="20" className={isBlocked("20", this.props.blockedTimes)}>
+                <tr id="20" className={activityType("20", this.props.blockedTimes)}>
                   <th>
                   8:00 PM
                   </th>
